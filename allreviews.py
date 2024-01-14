1945de5d70625c72729aa87045f21631750c269a
Shantanu Wani <127302217+Shaan2522@users.noreply.github.com>
2024-01-14 14:10:31 +0530

    Update allreviews.py

--- allreviews.py
+++ allreviews.py
@@ -4,7 +4,7 @@ import lxml
 
 import json
 
-headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10136'}
 
 l1 = []
 l2 = []
@@ -12,10 +12,24 @@ new_data = {}
 link = f"https://www.amazon.in/Amozo-Cover-iPhone-Polycarbonate-Transparent/product-reviews/B09J2MM5C6/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews&pageNumber=1"
 site = BeautifulSoup(requests.get(link,headers=headers).content,features="lxml")
 
+title = site.findAll("a", {"href" : "/Amozo-Cover-iPhone-Polycarbonate-Transparent/dp/B09J2MM5C6/ref=cm_cr_arp_d_product_top?ie=UTF8"})
+for i in range(1):
+    t = str(title[i]).split(">")[1].split("<")[0]
+new_data["PRODUCT NAME - "] = str(t)
+
+prod_link = site.findAll("a", {"data-hook" : "product-link"})
+z = "https://www.amazon.in/"
+z += str(prod_link).split("href=\"")[1].split("\">")[0]
+new_data["link to the product - "] = str(z)
+
+rating = site.findAll("span", {"class" : "a-size-medium a-color-base"})
+rating = str(rating).split("text\">")[1].split("<")[0]
+total_ratings = site.findAll("span", {"class" : "a-size-base a-color-secondary"})
+total_ratings = str(total_ratings).split("secondary\">")[1].split("<")[0]
+new_data[rating] = total_ratings
+
 top = site.findAll("span", {"class" : "a-size-base review-title a-text-bold"})
-# print(top)
 top_comment = site.findAll("div", {"class" : "a-row a-spacing-top-mini"})
-print(top_comment)
 for i in range(2):
     t = str(top[i]).split("-title\">")[1].split("<")[0]
     b = str(top_comment[i]).split("base\">")[1].split("<")[0]
